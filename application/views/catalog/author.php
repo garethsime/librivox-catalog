1ef8147fb226ba1207f336834d721415d6a745a1
Gareth Sime <garethsime@gmail.com>
2023-05-11 20:15:01 +1200

    Auto-format several PHP views
    This should make these files easier to read, and easier to modify
    without editors trying to automatically format them all the time.

--- application/views/catalog/author.php
+++ application/views/catalog/author.php
@@ -1,57 +1,42 @@
 <?= $header;  ?>
 
-
 <div class="main-content">
-	
-	
 	<?= $sidebar;  ?>
-	
-	
+
 	<div class="page author-page">
 		<div class="page-header-wrap js-header_section">
 			<div class="content-wrap clearfix">
-				<h1><?= format_author($author, FMT_AUTH_YEARS|FMT_AUTH_HTML)?></h1>
-				
+				<h1><?= format_author($author, FMT_AUTH_YEARS | FMT_AUTH_HTML) ?></h1>
+
 				<p class="description"><?= $author->blurb ?></p>
-				
+
 				<div class="page-header-half">
 					<p><span>External Links</span></p>
-					<?php 
-						if (!empty($author->author_url))
-						{
-							echo '<p>' . format_author($author, FMT_AUTH_WIKI) . '</p>';
-						}	
-					 	
-					 ?>				 
+					<?php
+					if (!empty($author->author_url)) {
+						echo '<p>' . format_author($author, FMT_AUTH_WIKI) . '</p>';
+					}
+					?>
 				</div>
-				
-				<div class="page-header-half">	
-					<p><span>Total matches:</span> <?= $matches ?></p>
-				</div>	
 
-			</div>	
-		
-			
+				<div class="page-header-half">
+					<p><span>Total matches:</span> <?= $matches ?></p>
+				</div>
+			</div>
+
 			<div class="sort-menu" id="sort_menu" style="display:none;">
 				<p>Order by</p>
 				<select class="js-sort-menu">
-					  <option value="alpha">Alphabetically</option>
-					  <option value="catalog_date">Release date</option>
-				 </select> 
+					<option value="alpha">Alphabetically</option>
+					<option value="catalog_date">Release date</option>
+				</select>
 			</div><!-- end .sort-menu -->
 		</div><!-- end . page-header -->
-			 		
-		<ul class="browse-list">
 
-					
-			
-		</ul>
-	
-	<div class="page-number-nav"></div>
-	
-	</div>
-</div><!-- end .page -->
+		<ul class="browse-list"></ul>
 
+		<div class="page-number-nav"></div>
+	</div><!-- end .page -->
 </div><!-- end .main-content -->
 
 <?= $footer;  ?>
